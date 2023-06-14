e48216b57f491b3eb43139e7a9b8cc7852823743
artemmyzrov <a.myzrov@yandex.ru>
2023-06-14 14:48:45 +0400

    fix: fixed bugs create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,13 +2,19 @@ import { promises as fsPromises } from 'fs'
 
 const create = async () => {
   const fileContent = 'I am fresh and young'
-  const filepath = 'src/fsfiles/fresh.txt'
+  const filepath = 'src/fs/files/fresh.txt'
 
-  fsPromises.writeFile(filepath, fileContent, (err) => {
-    if (err) throw err
-
-    console.log('The file was succesfully saved!')
-  })
+  try {
+    await fsPromises.access(filepath)
+    throw new Error('FS operation failed: File already exists')
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fsPromises.writeFile(filepath, fileContent)
+      console.log('The file was successfully saved!')
+    } else {
+      throw error
+    }
+  }
 }
 
 await create()
