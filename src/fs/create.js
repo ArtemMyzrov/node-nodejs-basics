e260e3025c19698ff769c3eb37bc3c9421b8a83f
artemmyzrov <a.myzrov@yandex.ru>
2023-06-14 13:16:51 +0400

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
-const create = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs'
 
-await create();
+const create = async () => {
+  const fileContent = 'I am fresh and young'
+  const filepath = 'src/fsfiles/fresh.txt'
+
+  fsPromises.writeFile(filepath, fileContent, (err) => {
+    if (err) throw err
+
+    console.log('The file was succesfully saved!')
+  })
+}
+
+await create()
