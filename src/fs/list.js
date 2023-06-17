bc72eaaeed10f22aac1832cd35d82e7af77014fd
artemmyzrov <a.myzrov@yandex.ru>
2023-06-17 17:11:29 +0400

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
-const list = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs'
 
-await list();
+const list = async () => {
+  try {
+    const dirPath = 'src/fs/files'
+    await fsPromises.access(dirPath)
+
+    const files = await fsPromises.readdir(dirPath)
+
+    console.log(files)
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Error: Directory does not exist. FS operation failed.')
+    } else {
+      throw error
+    }
+  }
+}
+
+await list()
