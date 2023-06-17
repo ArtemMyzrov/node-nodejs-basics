bc5183012b5d3cb3c16f46b40cd6a76f1445a046
artemmyzrov <a.myzrov@yandex.ru>
2023-06-17 16:42:35 +0400

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs'
 
-await rename();
+const rename = async () => {
+  try {
+    const sourceFile = 'src/fs/files_copy/wrongFilename.txt'
+    const targetFile = 'src/fs/files_copy/properFilename.md'
+
+    await fsPromises.access(sourceFile)
+
+    try {
+      await fsPromises.access(targetFile)
+      throw new Error('Error: Target file already exists.')
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        await fsPromises.rename(sourceFile, targetFile)
+      } else {
+        throw new Error('Error: Target file access failed.')
+      }
+    }
+
+    console.log('File renamed successfully!')
+  } catch (error) {
+    console.error('FS operation failed:', error)
+  }
+}
+
+await rename()
