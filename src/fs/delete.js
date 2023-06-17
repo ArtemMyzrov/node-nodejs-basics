2f777229478655b0d464b5f902cb79511e2bcfb6
artemmyzrov <a.myzrov@yandex.ru>
2023-06-17 17:05:53 +0400

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs'
 
-await remove();
+const remove = async () => {
+  const filePath = 'src/fs/files/fileToRemove.txt'
+
+  try {
+    await fsPromises.access(filePath)
+    await fsPromises.unlink(filePath)
+    console.log('The file was successfully deleted!')
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Error: File does not exist. FS operation failed.')
+    } else {
+      throw error
+    }
+  }
+}
+
+await remove()
