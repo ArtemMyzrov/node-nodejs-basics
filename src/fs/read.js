5c5987d33b65264efe1509a4f86bb907b5b2a97e
artemmyzrov <a.myzrov@yandex.ru>
2023-06-17 17:41:09 +0400

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
-const read = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs'
 
-await read();
+const read = async () => {
+  try {
+    const filesDir = 'src/fs/files/fileToRead.txt'
+    await fsPromises.access(filesDir)
+
+    const filesRead = await fsPromises.readFile(filesDir, 'utf-8')
+
+    console.log(filesRead)
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Error: Directory does not exist. FS operation failed.')
+    } else {
+      throw error
+    }
+  }
+}
+
+await read()
