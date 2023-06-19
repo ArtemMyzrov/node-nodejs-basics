ac071c5e0b57b69781d11352b6a6c6bb3d3803ab
artemmyzrov <a.myzrov@yandex.ru>
2023-06-19 16:41:44 +0400

    feat: add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import crypto from 'crypto'
+import fs from 'fs/promises'
 
-await calculateHash();
+const calculateHash = async () => {
+  const filePath = 'src/hash/files/fileToCalculateHashFor.txt'
+  try {
+    const readFile = await fs.readFile(filePath)
+    const hash = crypto.createHash('sha256').update(readFile).digest('hex')
+    console.log('Hash : ', hash)
+  } catch (error) {
+    console.log('File reading error:', error)
+  }
+}
+
+await calculateHash()
