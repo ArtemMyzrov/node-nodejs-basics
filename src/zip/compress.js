301b596f07292e9c7132c09be32e563d31365263
artemmyzrov <a.myzrov@yandex.ru>
2023-06-19 22:52:36 +0400

    feat: add completed zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
-const compress = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import zlib from 'zlib'
 
-await compress();
+const compress = async () => {
+  const inputPath = 'src/zip/files/fileToCompress.txt'
+  const outputPath = 'src/zip/archive.gz'
+
+  const readStream = fs.createReadStream(inputPath)
+  const writeStream = fs.createWriteStream(outputPath)
+  const zipStream = zlib.createGzip()
+
+  return new Promise((resolve, reject) => {
+    readStream
+      .pipe(zipStream)
+      .pipe(writeStream)
+      .on('finish', () => {
+        console.log('File compressed completed')
+        resolve()
+      })
+      .on('error', (error) => {
+        console.error('Error compressing file:', error)
+        reject(error)
+      })
+  })
+}
+
+await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import zlib from 'zlib'
 
-await decompress();
+const decompress = async () => {
+  const inputPath = 'src/zip/archive.gz'
+  const outputPath = 'src/zip/files/fileToCompressss.txt'
+
+  const readStream = fs.createReadStream(inputPath)
+  const writeStream = fs.createWriteStream(outputPath)
+
+  const unzipStream = zlib.createGunzip()
+
+  readStream.pipe(unzipStream).pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
+      console.log('Decompression compled')
+      resolve()
+    })
+
+    writeStream.on('error', (error) => {
+      console.error('Error decompressing file:', error)
+      reject(error)
+    })
+  })
+}
+
+await decompress()
