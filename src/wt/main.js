83b56ef35801ccc482d25d2426eef604c8be2c86
artemmyzrov <a.myzrov@yandex.ru>
2023-06-19 23:40:45 +0400

    feat: add completed worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'worker_threads'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { cpus } from 'os'
 
-await performCalculations();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const workerPath = join(__dirname, 'worker.js')
+
+const performCalculations = async () => {
+  const cores = cpus()
+  const results = []
+  const res = await Promise.allSettled(
+    cores.map(
+      (_, i) =>
+        new Promise((resolve, reject) => {
+          const n = 10 + i
+          const worker = new Worker(workerPath, {
+            workerData: n,
+          })
+          worker.on('message', (message) => {
+            results.push({ status: 'resolved', data: message.result })
+            resolve(message)
+          })
+          worker.on('error', (message) => {
+            reject(message)
+          })
+        })
+    )
+  )
+
+  for (const { status, value } of res) {
+    console.log({
+      status: status === 'fulfilled' ? 'resolved' : 'error',
+      data: value || null,
+    })
+  }
+}
+
+await performCalculations()
